bd74a97b44f58609c06e4478d6eeffd91719a3a0
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-12 17:23:14 +0530

    Madhu

--- set1.py
+++ set1.py
@@ -1,18 +1,24 @@
 #Problem-1 ==>Sum of elements in a list
+
 '''list=[1,2,3,4,5]
 sum=0
 for i in list:
     sum+=i
 print(sum)'''
+
+# Output: 15
 #------------------------------
 
 #Problem-2 ==>Find the largest number in a list
+
 '''list=[4,6,8,9,3,2]
 print(max(list))'''
 
+# Output: 9
 #-------------------------------
 
 #Problem-3 ==>Count even numbers in a list
+
 '''list=[1,2,3,4,5,6,7,8,9]
 count=0
 for i in list:
@@ -20,20 +26,23 @@ for i in list:
         count+=1
 print(count)'''
 
+# Output: 4
 #---------------------------------
 
 #Problem-4  ==>Remove duplicates from a list
+
 '''list=[1,2,3,4,1,2,3,4]
 uniq=[]
-
 for i in list:
     if i not in uniq:
         uniq.append(i)
 print(uniq)'''
 
+# Output: [1, 2, 3, 4]
 #-----------------------------------
 
 #Problem-5 ==>Find the secound largest number
+
 '''list=[5,1,9,6]
 max=max(list)
 
@@ -41,9 +50,12 @@ for i in range(max-1,0,-1):
     if i in list:
         print(i)
         break'''
+
+# Output: 6
 #-------------------------------------
 
 #Problem-6 ==>Count word occurence in a sentence
+
 '''str="apple banana apple"
 list=str.split(" ")
 dict={}
@@ -51,6 +63,8 @@ for i in list:
     var=list.count(i)
     dict[i]=var
 print(dict)'''
+
+# Output: {'apple': 2, 'banana': 1}
 #---------------------------------------
 
 #Problem-7 ==>Update values of a key in a dictionary
@@ -61,24 +75,30 @@ print(dict)'''
 #----------------------------------------
 
 #Problem-8 ==>Check if a key exists in a dictionary
+
 '''dict={'x':5,'y':6}
 key='x'
 for i in dict:
     if i==key:
-        print("True")ss
+        print("True")
         break
     else:
         print("False")
         break'''
+        
+# Output: True
 #-------------------------------------
 
 #Problem-9 ==>Print only dictionary keys
+
 '''dict={'name':'avr','age':21}
-print(dict.values())
-'''
+print(dict.values())'''
+
+# Output: (['avr', 21])
 #-------------------------------------
 
 #Problem-10 ==>Add new key-value if key doesn't exist
+
 '''dict={'x':1}
 key_val={'y':2}
 
@@ -91,25 +111,35 @@ for i in dict:
 if flag==True:
     dict[j]=key_val[j]
 print(dict)'''
+
+# Output: {'x': 1, 'y': 2}
 #-------------------------------------
 
 #Problem-11 ==>Check if a number is even or odd
+
 '''num=9
 if num%2==0:
     print("Even")
 else:
     print("Odd")'''
+    
+# Output: Odd
+
 #--------------------------------------
 
 #Problem-12 ==>Find factorial of a number
+
 '''num=5
 fact=1
 for i in range(1,num+1):
     fact=fact*i
 print(fact)'''
+
+# Output: 120
 #---------------------------------------
 
 #Problem-13 ==>Check if a number is prime
+
 '''num=13
 count=0
 for i in range(2,num,1):
@@ -118,26 +148,33 @@ for i in range(2,num,1):
 if count>0:
     print("Not a Prime")
 else:
-    print("Prime")
-'''
+    print("Prime")'''
+
+# Output: Prime
 #--------------------------------------
 
 #Problem-14 ==>Reverse the digit of a number
+
 '''num=1234
-temp=1
+temp=0
 while num!=0:
     quo=num%10
     temp=(temp*10)+quo
     num=num//10
 print(temp)'''
+
+# Output: 4321
 #-----------------------------------
 
 #Problem-15 ==>Count the Digits of a number
+
 '''num=789
 count=0
 while num!=0:
     num=num//10
     count+=1
 print(count)'''
+
+# Output: 3
 #-----------------------------------
 
--- set2.py
+++ set2.py
@@ -1,15 +1,20 @@
 #Problem-1 ==>Multiply all elements in a list
+
 '''list=[2,3,4]
 mul=1
 for i in list:
     mul=mul*i
 print(mul)'''
+
+#Output: 24
 #--------------------------------------------
 
 #Problem-2 ==>Find minimum value in a list
 
 '''list=[8,3,5]
 print(min(list))'''
+
+#Output:3
 #---------------------------------------------
 
 #Problem-3 ==>Find count of add numbers in a list
@@ -19,8 +24,9 @@ count=0
 for i in list:
     if i%2==0:
         count+=1
-print(count)
-'''
+print(count)'''
+
+#Output:2
 #----------------------------------------------
 
 #Problem-4 ==> Find difference between max and min
@@ -30,6 +36,7 @@ max=max(list)
 min=min(list)
 print(f"Difference of {min} and {max} is {max-min}")
 '''
+#Output: Difference of 2 and 9 is 7
 #--------------------------------------------------
 
 #Problem-5 ==>Sum of only Positive numbers
@@ -41,6 +48,7 @@ for i in list:
         sum=sum+i
 print(sum)'''
 
+#Output: 8
 #----------------------------------------------------
 
 # Problem-6 ==>Get keys with even values from dict
@@ -50,8 +58,9 @@ keys=[]
 for i in dic:
     if dic[i]%2==0:
         keys.append(i)
-print(keys)
-'''
+print(keys)'''
+
+# Output: 
 #-----------------------------------------------------
 
 # problem-7 ==>invert dictionary (keys become values)
@@ -61,7 +70,9 @@ dic2={}
 
 for i in dic1:
     dic2[dic1[i]]=i
-print(dic2) '''
+print(dic2)'''
+
+# Output: {1: 'x', 2: 'y'}
 #------------------------------------------------------
 
 #problem-8 ==>Check if two dictionaries are equal
@@ -69,14 +80,18 @@ print(dic2) '''
 '''dic1={'a':1}
 dic2={'a':1}
 print(dic1==dic2)'''
-#-------------------------------------------------------
 
+# Output: True
+#-------------------------------------------------------
 #problem=9  ==>Sum all values in dictionary
+
 '''dic={'a':5,'b':10}
 sum=0
 for i in dic:
     sum=sum+dic[i]
 print(sum)'''
+
+# Output: 15
 #--------------------------------------------------------
 
 #problem-10  ==>List all values greater than 10
@@ -87,6 +102,8 @@ for i in dic:
     if dic[i]>10:
         res.append(dic[i])
 print(res)'''
+
+# Output: [12, 15]
 #---------------------------------------------------------
 
 #Problem-11 ==>Check if number is positive ,negative or zero
@@ -97,11 +114,13 @@ if num==0:
 elif num>0:
     print("Postive")
 else:
-    print("Negative")
-'''
+    print("Negative")'''
+    
+#Output: Negative
 #----------------------------------------------------------
 
 # Problem-12 ==>Generate first 5 even numbers
+
 '''n=5
 even_no=[]
 i=1
@@ -114,18 +133,23 @@ while True:
     if count==n:
         break
 print(even_no)'''
+
+# Output: [2, 4, 6, 8, 10]
 #-------------(or)-------------------------
 '''n=5
 lis=[]
 for i in range(2,n*2,2):
     lis.append(i)
 print(lis)'''
+#Output: [2, 4, 6, 8, 10]
 #---------------------------------------------
 
 #Problem-13 ==>Find cube of a number 
 
 '''n=3
 print(n**3)'''
+
+# Output: 27
 #-----------(or)------------
 
 '''n=3
@@ -133,6 +157,8 @@ cube=1
 for i in range(1,4):
     cube=cube*n
 print(cube)'''
+
+#Output: 27
 #-----------------------------
 
 # Problem-14 ==>Check if number is multiple of 7
@@ -142,6 +168,8 @@ if num%7==0:
     print("Yes")
 else:
     print(False)'''
+    
+# Output: False
 #--------------------------------
 
 # Problem-15 ==> Convert number to string and reverse it
@@ -149,9 +177,10 @@ else:
 '''n=123
 res=str(n)
 str=res[::-1]
-print(f"'{str}'")
-'''
+print(f"'{str}'")'''
 
+# Output: '321'
+#------------------------------------
 
 
 
