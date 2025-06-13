7e697ab4d5a6ce0e3d8ee7b8037552cd4014a4d2
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-13 15:51:14 +0530

    Update problem-solving.py

--- problem-solving.py
+++ problem-solving.py
@@ -97,14 +97,35 @@ var="aaaabbbccddggjj"
 #     print(new_lis[i],new_lis[i+1])
 #     i+=2
         
-lis=[2,8,9,4,5,3]
-max_num=max(lis)
-lis.remove(max_num)
-print(max(lis))
+# lis=[2,8,9,4,5,3]
+# max_num=max(lis)
+# lis.remove(max_num)
+# print(max(lis))
 
-
+# lis=[2,8,9,4,5,3]
     
-        
+
+
+# output=[x  for x in range(10) if x%2==0]  
+# print(output)   
+
+
+
+
+# user=int(input("Enter the number of items"))
+# di={}
+# for i in range(user):
+#     key,val=input("Enter the key and val ").split()
+#     di[key]=val
+# print(di)
+
+
+# list Comprehension
+user=input("madhukiran")
+li=[i for i in user]
+print(li)
+    
+    
     
 
 
