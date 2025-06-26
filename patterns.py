5ce7d0f9e0951a041b10e40b8839afe9af71f2e7
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-26 20:11:07 +0530

    ps

---
+++ patterns.py
@@ -0,0 +1,215 @@
+# 1)
+# n=6
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or i==n-1 or j==0 or j==n-1:
+#             print("*",end=" ") 
+#         else:
+#             print(" ",end=" ")
+            
+#     print("")
+#------------------------------------------    
+# 
+# 2)
+# n=4  
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if j==1:
+#             print(" "*(n-i),end=" ")
+#         print(i,end=" ")
+        
+#     print("")
+#----------------------------------------
+# 
+# 3)
+# n=4
+# for  i in range(n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print("")
+#-----------------------------------------
+
+# 4)
+# n=4
+
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print("")
+    
+#---------------------------------------
+# 5)
+# n=4
+# k=1
+# for i in range(n):
+#     for j in range(0,i+1):
+        
+#         print(k,end=" ")
+#         k=k+1
+#     print("")
+#--------------------------------------
+# 6)
+# n=4
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if (i+j)%2==0:
+#             print("1",end=" ")
+#         else:
+#             print("0",end=" ")
+#     print("")
+
+#---------------------------------------
+# 7)
+# n=4
+
+# for i in range(n)
+
+#------------------------------
+# #8)
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         if j==0:
+#             print(" "*i,end="")
+#         print("*",end=" ")
+#     print("")
+#-------------------------------
+#9)
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         if j==0:
+#             print(" "*(n-i),end="")
+#         print("*",end=" ") 
+#     print("")
+# for i in range(n-2,-1,-1):
+#     for j in range(i+1):
+#         if j==0:
+#             print(" "*(n-i),end="")
+#         print("*",end=" ")
+#     print("")
+
+#----------------------------------------
+# 10
+# n=7
+# for j in range(n):
+#     for j in range(n):
+#         print("*")
+        
+        
+#---------------------------------------
+# 11)
+# n=6
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print("")
+#---------------------------------------
+# 12)
+
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print("")
+
+#---------------------------------------
+# 13)
+# n=5
+# for i in range(n,-1,-1):
+#     for j in range(i):
+#         print("*",end=" ") 
+#     print("")
+
+#----------------------------------------
+# 14)
+# n=5
+
+# for i in range(n+1):
+#     for j in range(i):
+#         if j==0:
+#             print("  "*(n-i),end="")
+#         print("*",end=" ")
+#     print("")
+
+#--------------------------------------
+#15
+# n=5
+# for i in range(n,-1,-1):
+#     for j in range(i):
+#         if j==0:
+#             print("  "*(n-i),end="")
+#         print("*",end=" ")
+#     print("")
+#--------------------------------------
+#16
+
+# n=4
+
+# for i in range(n,0,-1):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print("")
+    
+# for i in range(n+1):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print("")
+
+#--------------------------------
+#17
+
+# n=5
+
+# for i in range(n):
+#     for j in range(i):
+#         if j==0:
+#             print(" "*(n-i),end="")
+#         print("*",end=" ")
+#     print("")
+
+#------------------------------------
+#18
+
+# n=4
+
+# for i in range(n,-1,-1):
+#     for j in range(i):
+#         if j==0:
+#             print(" "*(n-i),end="")
+#         print(j+1,end=" ")
+#     print("")
+
+#-----------------------------------
+# 19)
+
+
+
+
+
+
+
+#-------------------------------------
+# 20)
+
+n=5
+
+for i in range(n+1):
+    for j in range(i):
+        if j==0:
+            print(" "*(n-i),end=" ")
+        if (i>=2 and i<n and j>0 and j<(i-1)) :
+            print(" ",end=" ") 
+        else: 
+            print("*",end=" ")
+    print("")
+    
+
+
+
+
+
+
+
+
